068431e0b062bcf9dd5134b4d4fa2161e5309c98
lardedah <tomholbrook94@gmail.com>
2022-06-13 10:00:48 +0100

    t te tes test est st t

--- loginscreen.py
+++ loginscreen.py
@@ -1,34 +1,36 @@
-username = ("Michael")
-password = ("Jackson")
+#username = ("Michael")
+#password = ("Jackson")
 
-username_attempt = input ("Please enter your username: ")
-while username_attempt != username:
-    username_attempt = input("Please enter your username: ")
+#username_attempt = input ("Please enter your username: ")
+#while username_attempt != username:
+#    username_attempt = input("Please enter your username: ")
 
-password_attempt = input ("Please enter your password: ")
-while password_attempt != password:
-    password_attempt = input("Please enter your password: ")
+#password_attempt = input ("Please enter your password: ")
+#while password_attempt != password:
+#    password_attempt = input("Please enter your password: ")
 
-print ("Welcome...          \n \n " + "Come in, " + str(username))
+#print ("Welcome...          \n \n " + "Come in, " + str(username))
 
 #### #### #### #### #### alternative: #### #### #### #### ####
 
-username = "Tom"
-password = "Holbrook"
+#username = "Tom"
+#password = "Holbrook"
 
-username_attempt = input ("Username: ")
+#username_attempt = input ("Username: ")
 
-while not username_attempt == username:
-    print ("Username not recognised")
-    username_attempt = input("Username: ")
-else:
-    print("Please provide password ")
+#while not username_attempt == username:
+#    print ("Username not recognised")
+#    username_attempt = input("Username: ")
+#else:
+#    print("Please provide password ")
 
-password_attempt = input ("")
+#password_attempt = input ("")
 
-if password_attempt == password:
-    print ("Welcome.")
-else:
-    print ("Access denied.")
+#if password_attempt == password:
+#    print ("Welcome.")
+#else:
+#    print ("Access denied.")
 
-#### #### #### ####     #### #### #### ####
+#### #### #### #### #### alternative: #### #### #### #### ####
+
+
---
+++ remind to bring for loop.py
@@ -0,0 +1,7 @@
+def remind(item):
+    print("Remember to bring your " + item)
+
+things = ["bag", "toothbrush", "towel"]
+
+for i in things:
+    remind (i)
